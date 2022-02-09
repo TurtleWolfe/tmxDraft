09504c859ebf90dca85f05b150820dda6c44df9e
TurtleWolfe <7327259+TurtleWolfe@users.noreply.github.com>
2022-02-09 18:10:12 -0500

    AppBar & MuiCard

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
-
+import AppBar from './components/AppBar';
+import MuiCard from './components/MuiCard';
+import Button from '@mui/material/Button';
 function App() {
   return (
     <div className="App">
+      <AppBar></AppBar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h4>
+
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Button
+              variant="contained"
+            >
+              Learn React
+            </Button>
+
+          </a>
+        </h4>
+
+        <MuiCard>
+
+        </MuiCard>
       </header>
     </div>
   );
